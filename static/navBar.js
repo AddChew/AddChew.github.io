96b67bb734fd0ed0ccc54afd49a6c18efdeb9632
AddChew <56240071+AddChew@users.noreply.github.com>
2021-10-28 16:19:08 +0800

    added functional media query

--- static/navBar.js
+++ static/navBar.js
@@ -2,12 +2,25 @@
 let get_basename = path => {
     const index = path.lastIndexOf("/") + 1
     return path.substring(index)
-}   
+}
+
+// Function to set active tab
+let set_active_tab = () => {
+    const url = get_basename(window.location.pathname)
+    const navItem = document.querySelector(`li a[href="${url}"]`)
+    navItem.classList.add("active")
+}
+
+// Function to toggle display of navbar
+let toggle_navbar = () => {
+    const navbar = document.querySelector(".nav-bar")
+    // console.log(navbar)
+    navbar.classList.toggle("nav-bar-active")
+}
 
 // Set the corresponding navigation tab to be active based on basename
 document.addEventListener("DOMContentLoaded", () => {
-    const url = get_basename(window.location.pathname)
-    const navItem = document.querySelector(`li a[href="${url}"]`)
-    console.log(navItem)
-    navItem.classList.add("active")
+    const navbutton = document.querySelector(".nav-bar-button")
+    navbutton.addEventListener("click", toggle_navbar)
+    set_active_tab()
 })
