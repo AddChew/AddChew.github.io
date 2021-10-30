cd0a591bf8b390a5b7887aff734d9c49ca65fea3
AddChew <56240071+AddChew@users.noreply.github.com>
2021-10-30 18:40:49 +0800

    fixed path issues

--- static/appscripts/navBar.js
+++ static/appscripts/navBar.js
@@ -1,12 +1,6 @@
-// Function to extract the basename of a url path
-let get_basename = path => {
-    const index = path.lastIndexOf("/") + 1
-    return path.substring(index)
-}
-
 // Function to set active tab
 let set_active_tab = () => {
-    const url = get_basename(window.location.pathname)
+    const url = window.location.pathname === "/" ? "/index.html" : window.location.pathname
     const navItem = document.querySelector(`li a[href="${url}"]`)
     navItem.classList.add("active")
 }
