5990edffe3b892b4ed3441a98339b34632e0132b
AddChew <56240071+AddChew@users.noreply.github.com>
2021-10-30 18:24:40 +0800

    froze files

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_frozen import Freezer
+
 
 app = Flask(__name__)
+freezer = Freezer(app)
+
 
 @app.route("/")
 def index():
-    return redirect(url_for("navigate_to", page = "about.html"))
+    return redirect(url_for("navigate_to", page = "index.html"))
 
 
 @app.route("/<string:page>")
 def navigate_to(page: str):
     return render_template(page)
+
+
+if __name__ == "__main__":
+    # app.run()
+    freezer.freeze()
